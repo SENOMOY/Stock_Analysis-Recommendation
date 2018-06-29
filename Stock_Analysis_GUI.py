8cd7e18abd0ed133b96901f5e5a4340de9fdb570
SENOMOY <subhajitsen220188@gmail.com>
2018-06-30 01:11:13 +0530

    Alphabet options

--- Stock_Analysis_GUI.py
+++ Stock_Analysis_GUI.py
@@ -1,11 +1,13 @@
 from tkinter import *
 from tkinter import messagebox
+import pandas as pd
 import tkinter as tk
 from data.Company_Data import getCompanyData
 from report.GenerateReport import generateReport
+import string
 
 root = Tk()
-root.title("Stock Analysis")
+root.title("Stock Analysis & Prediction")
 
 # Add a grid
 mainframe = Frame(root)
@@ -36,15 +38,26 @@ popupMenu2.config(width=13, indicatoron=0, padx=10, state=DISABLED)
 Label(mainframe, text="Exchange").grid(row=1, column=2)
 popupMenu2.grid(row=2, column=2)
 
+# Create alphabet tkinter variable
+alphabet_tnktr = StringVar(root)
+# Set the default option
+alphabet_tnktr.set('A-Z')
+# Create alphabet option menu
+popupMenu3 = OptionMenu(mainframe, alphabet_tnktr, '')
+popupMenu3.config(width=3, indicatoron=0, padx=10, state=DISABLED)
+Label(mainframe, text="A-Z").grid(row=1, column=3)
+popupMenu3.grid(row=2, column=3)
+
+
 # Create company tkinter variable
 company_tnktr = StringVar(root)
 # Set the default option
 company_tnktr.set('Select Company')
 # Create exchange option menu
-popupMenu3 = OptionMenu(mainframe, company_tnktr, '')
-popupMenu3.config(width=13, indicatoron=0, padx=10, state=DISABLED)
-Label(mainframe, text="Company").grid(row=1, column=3)
-popupMenu3.grid(row=2, column=3)
+popupMenu4 = OptionMenu(mainframe, company_tnktr, '')
+popupMenu4.config(width=20, indicatoron=0, padx=10, state=DISABLED)
+Label(mainframe, text="Company").grid(row=1, column=4)
+popupMenu4.grid(row=2, column=4)
 
 
 def resetOptions(menu, tnktr, msg):
@@ -53,14 +66,17 @@ def resetOptions(menu, tnktr, msg):
 
 def getAdvisory():
     company_list = getCompanyData([country_tnktr.get()],[exchange_tnktr.get()]).sort_values('Country',ascending=True)
-    company_ticker = company_list.loc[(company_list['Name'].isin([company_tnktr.get()]))]['Ticker']\
-    .dropna().unique()[0]
+    company_ticker = company_list.loc[(company_list['Name'].isin([company_tnktr.get()]))]['Ticker'] \
+        .dropna().unique()[0]
     messagebox.showinfo('','Running Stock Advisory for '+company_tnktr.get()+' from ' + exchange_tnktr.get()
                         + ' exchange of ' + country_tnktr.get())
     generateReport(company_ticker, company_tnktr.get())
     resetOptions(popupMenu2, exchange_tnktr, 'Select Exchange')
+    resetOptions(popupMenu3, alphabet_tnktr, 'A-Z')
+    resetOptions(popupMenu4, company_tnktr, 'Select Company')
     popupMenu2.config(state=DISABLED)
     popupMenu3.config(state=DISABLED)
+    popupMenu4.config(state=DISABLED)
     advisory_button.config(state=DISABLED)
 
 advisory_button = Button(mainframe, text="Get Advisory", state=DISABLED, command=getAdvisory, bg="black", fg="white")
@@ -70,32 +86,44 @@ advisory_button.grid(row=5, column=2)
 def enableExchange(*args):
     resetOptions(popupMenu2, exchange_tnktr, 'Select Exchange')
     exchange_tnktr.set('Select Exchange')
-    exchange = getCompanyData([country_tnktr.get()],[]).sort_values('Exchange',ascending=True)['Exchange']\
+    exchange = getCompanyData([country_tnktr.get()],[]).sort_values('Exchange',ascending=True)['Exchange'] \
         .dropna().unique()
     popupMenu2.config(state=NORMAL)
     popupMenu3.config(state=DISABLED)
+    popupMenu4.config(state=DISABLED)
     advisory_button.config(state=DISABLED)
     for value in exchange:
         popupMenu2['menu'].add_command(label=value, command=tk._setit(exchange_tnktr, value))
 
+def enableAlphabetOptions(*args):
+    resetOptions(popupMenu3, alphabet_tnktr, 'A-Z')
+    alphabet_tnktr.set('A-Z')
+    alphabets = list(string.ascii_uppercase)
+    popupMenu3.config(state=NORMAL)
+    popupMenu4.config(state=DISABLED)
+    for value in alphabets:
+        popupMenu3['menu'].add_command(label=value, command=tk._setit(alphabet_tnktr, value))
 
 # on change dropdown value
 def enableCompany(*args):
-    resetOptions(popupMenu3, company_tnktr, 'Select Company')
+    resetOptions(popupMenu4, company_tnktr, 'Select Company')
     company_tnktr.set('Select Company')
-    company = getCompanyData([country_tnktr.get()],[exchange_tnktr.get()]).sort_values('Name',ascending=True)['Name']\
+    company = getCompanyData([country_tnktr.get()],[exchange_tnktr.get()]).sort_values('Name',ascending=True)['Name'] \
         .dropna().unique()
-    popupMenu3.config(state=NORMAL)
+    company_srs = pd.Series(company)
+    company_srs = company_srs.loc[company_srs.str.startswith(alphabet_tnktr.get(), na=False)]
+    popupMenu4.config(state=NORMAL)
     advisory_button.config(state=DISABLED)
-    for value in company:
-        popupMenu3['menu'].add_command(label=value, command=tk._setit(company_tnktr, value))
+    for value in company_srs:
+        popupMenu4['menu'].add_command(label=value, command=tk._setit(company_tnktr, value))
 
 def enableButton(*args):
     advisory_button.config(state=NORMAL)
 
 # link function to change country dropdown
 country_tnktr.trace('w', enableExchange)
-exchange_tnktr.trace('w', enableCompany)
+exchange_tnktr.trace('w', enableAlphabetOptions)
+alphabet_tnktr.trace('w',enableCompany)
 company_tnktr.trace('w', enableButton)
 
 root.mainloop()
--- report/GenerateReport.py
+++ report/GenerateReport.py
@@ -7,8 +7,11 @@ from data.Twitter_Data import getSentimentResult
 def generateReport(company_ticker,company):
 	flag = build_stock_dataset([company_ticker])
 	if flag:
+		print('Running Time Series Analysis...')
 		generateTimeSeriesPlot(company_ticker,company)
+		print('Running Linear Regression Prediction...')
 		linearRegressionPrediction(company_ticker,company)
+		print('Running Sentiment Analysis...')
 		getSentimentResult(company)
 	else:
 		messagebox.showinfo('Error fetching data', 'No data available for ' + company)
